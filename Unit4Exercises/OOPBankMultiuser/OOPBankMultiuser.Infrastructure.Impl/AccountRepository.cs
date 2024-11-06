2c0f4ebb27442a78ca7404dabea3c118d7da96f8
isa-sanchez-vuel <isabela.sanchez@col.vueling.com>
2024-11-06 14:47:54 +0100

    more fixes

--- Unit4Exercises/OOPBankMultiuser/OOPBankMultiuser.Infrastructure.Impl/AccountRepository.cs
+++ Unit4Exercises/OOPBankMultiuser/OOPBankMultiuser.Infrastructure.Impl/AccountRepository.cs
@@ -7,51 +7,13 @@ namespace OOPBankMultiuser.Infrastructure.Impl
 {
 	public class AccountRepository : IAccountRepository
 	{
-		/*private static List<Account> simulatedAccountDBTable = new()
-		{
-			new()
-			{
-				Name = "Pepito Grillo",
-				Iban = "ES54 3000 4790 54 0000000000",
-				IdNumber = 1,
-				Pin = "1111",
-				Balance = 500.0m
-			},
-			new()
-			{
-				Name = "Francisco Bezerra",
-				Iban = "ES54 3000 4790 54 0000000001",
-				IdNumber = 2,
-				Pin = "2222",
-				Balance = 50.0m
-			},
-			new()
-			{
-				Name = "Laura Bastión",
-				Iban = "ES54 3000 4790 54 0000000002",
-				IdNumber = 3,
-				Pin = "3333",
-				Balance = 1000.0m
-			},
-		};*/
+		
 
 		private readonly OOPBankMultiuserContext _context = new();
 
 		public AccountRepository(OOPBankMultiuserContext DBContext)
 		{
 			_context = DBContext;
-		}
-
-		private static int CurrentLoggedAccount = 1; 
-
-		public void SetCurrentAccount(int accountNumber)
-		{
-			CurrentLoggedAccount = accountNumber;
-		}
-
-		public int GetCurrentLoggedId()
-		{
-			return CurrentLoggedAccount;
 		}
 
 		public bool AccountExists(int id)
